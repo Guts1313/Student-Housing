67627d9414876b0b1d46c61069405aca5872af2e
Danila <dansil2301@gmail.com>
2023-05-15 10:52:09 +0200

    documents + main menu

---
+++ StudentHousing/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentHousing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            showAdmibTab();
+        }
+
+        public void showAdmibTab()
+        {
+            Style borderMarginStyle = FindResource("BorderMargin") as Style;
+            Setter marginSetter = borderMarginStyle.Setters.FirstOrDefault() as Setter;
+            marginSetter.Value = new Thickness(10, 11, 10, 0);
+
+            foreach (TabItem item in MenuTabs.Items)
+            {
+                if(item.Header.ToString() == "Admin")
+                    item.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem selectedItem = e.AddedItems[0] as TabItem;
+            string selectedHeader = selectedItem.Header.ToString();
+            TabName.Text = selectedHeader;
+        }
+    }
+}
