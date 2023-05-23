1c043309f79bbe9ce7cc4e9d53509f43ff74d3d1
Danila <dansil2301@gmail.com>
2023-05-23 20:30:38 +0200

    groceries design + login with google

--- StudentHousing/App.xaml.cs
+++ StudentHousing/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Auth.UI;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +11,24 @@ using System.Windows;
 
 namespace StudentHousing
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            FirebaseUI.Initialize(new FirebaseUIConfig
+            {
+                ApiKey = "AIzaSyBwrjUhnL_EiJnG9lOLFMLUZsA-jxRMKWo",
+                AuthDomain = "student-auth-95a22.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new GoogleProvider(),
+                    new EmailProvider()
+                },
+                PrivacyPolicyUrl = "https://github.com/step-up-labs/firebase-authentication-dotnet",
+                TermsOfServiceUrl = "https://github.com/step-up-labs/firebase-database-dotnet",
+                IsAnonymousAllowed = false,
+                UserRepository = new FileUserRepository("StudentHousing")
+            });
+        }
     }
 }
--- StudentHousing/MainWindow.xaml.cs
+++ StudentHousing/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Firebase.Auth.UI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using Firebase.Auth.UI;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,26 +23,55 @@ namespace StudentHousing
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> MyDataItems { get; set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
             showAdmibTab();
+            MyDataItems = new ObservableCollection<string>();
+            FirebaseUI.Instance.Client.AuthStateChanged += this.AuthStateChanged;
         }
 
+        // checks if the user is logged in
+        private void AuthStateChanged(object sender, Firebase.Auth.UserEventArgs e)
+        {
+            if (e.User == null)
+            {
+                Window myWindow = new LoginPage();
+                myWindow.Show();
+                this.Close();
+            }
+            else // all functions calls should be here (probably event manager will be here)
+            {
+                this.Show();
+                MessageBox.Show($"Hello, {e.User.Info.LastName}");
+            }
+        }
+
+        // moves all the elements in the window so that an admin panel fits
         public void showAdmibTab()
         {
-            Style borderMarginStyle = FindResource("BorderMargin") as Style;
+            Style borderMarginStyle = (Style)FindResource("BorderMargin");
             Setter marginSetter = borderMarginStyle.Setters.FirstOrDefault() as Setter;
-            marginSetter.Value = new Thickness(10, 11, 10, 0);
 
+            marginSetter.Value = new Thickness(10, 11, 10, 0);
             adminItem.Visibility = Visibility.Visible;
         }
 
+        // shows the name of the selected tab
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             TabItem selectedItem = e.AddedItems[0] as TabItem;
             string selectedHeader = selectedItem.Header.ToString();
             TabName.Text = selectedHeader;
+        }
+
+        // prevents default action of the listbox (it always wants to do smth when item is clicked)
+        private void PreventDef(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
