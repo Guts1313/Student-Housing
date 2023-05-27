b96b863e7e74019f732ec86af7dd9037fd0aa0e4
Angel Rusev <i517171@fhict.nl>
2023-05-27 20:13:38 +0200

    I created a raw basic version of most classes with basic UI for testing

--- StudentHousing/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ StudentHousing/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -118,7 +118,7 @@ namespace StudentHousing {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/StudentHousing;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/StudentHousing;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
